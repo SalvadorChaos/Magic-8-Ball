decb3b2f7bf21592b0a073f8f7cbf3312b8b019c
Salvador Martinez Jr <salvadormartinez022693@gmail.com>
2020-03-27 18:17:34 -0500

    Initial commit

---
+++ android/app/src/main/java/com/salvadorchaos/magic_8_ball/MainActivity.java
@@ -0,0 +1,13 @@
+package com.salvadorchaos.magic_8_ball;
+
+import android.support.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Center(
+          child: Text(
+            'Ask Me Anything',
+          ),
+        ),
+      ),
+      backgroundColor: Colors.deepPurpleAccent,
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: FlatButton(
+          onPressed: () {
+            setState(() {
+              ballNumber = Random().nextInt(9) + 1;
+            });
+          },
+          child: Image(
+            image: AssetImage('images/ball$ballNumber.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
